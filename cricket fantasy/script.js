32d02a087100a37f49762a41dcf21d9faab28b9b
riyan maredia <91208899+riyanmaredia12@users.noreply.github.com>
2023-10-04 12:38:13 +0500

    Add files via upload

---
+++ cricket fantasy/script.js
@@ -0,0 +1,34 @@
+// Sample data baki replace karna hoga manually
+const teamData = [
+    { ranking: 1, teamName: 'Team A', owner: 'Owner A', points: 20 },
+    { ranking: 2, teamName: 'Team B', owner: 'Owner B', points: 18 },
+    { ranking: 3, teamName: 'Team C', owner: 'Owner C', points: 16 },
+    { ranking: 4, teamName: 'Team D', owner: 'Owner D', points: 14 },
+    { ranking: 5, teamName: 'Team E', owner: 'Owner E', points: 12 },
+    // Add more teams as needed
+];
+
+// yeh bsdka function hai yeh hata to fantasy ki gaand phati
+function displayTeams() {
+    const resultTable = document.getElementById('resultTable');
+
+    teamData.forEach((team) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${team.ranking}</td>
+            <td>${team.teamName}</td>
+            <td>${team.owner}</td> 
+            <td>${team.points}</td>
+        `;
+        resultTable.appendChild(row);
+    });
+}
+
+// yahan se function ko call karlena baat karli hai display karlega yeh
+window.onload = displayTeams;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const body = document.querySelector("body");
+    body.classList.add("animate");
+});
+
